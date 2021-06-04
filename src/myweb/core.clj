49bef515ab8ed3848427cb79c738f115b537b1f8
Jorge Duenas <jorge@duenas.at>
2021-06-05 00:50:04 +0200

    a little refactoring

--- src/myweb/core.clj
+++ src/myweb/core.clj
@@ -12,14 +12,21 @@
       (into {} params)))
 
 
+(defn http-response
+  "Create an HTTP response for `request` containing `contents` as body"
+  [request contents]
+  (let [origin ((request :headers) "origin")]   ;TODO: map in map
+    {:body contents
+     :status 200
+     :headers {"Content-Type" "text/plain"
+               "Access-Control-Allow-Origin" origin
+               "Access-Control-Allow-Credentials" "true"}}))
+
+
 (defn myapp [request]
   (let [params (parse-query-string (:query-string request))]
     (println "Got a request with following parameters: " (str params))
-    {:body (format "Hello World %s and %s" (params "str1") (params "str2"))
-     :status 200
-     :headers {"Content-Type" "text/plain"
-               "Access-Control-Allow-Origin" ((request :headers) "origin")
-               "Access-Control-Allow-Credentials" "true"}}))
+    (http-response request (format "Hello World %s and %s" (params "str1") (params "str2")))))
 
 
 (defn -main []
