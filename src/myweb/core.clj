7cd1f13d742f7f50bb708f49b19d40e19ce4a9c8
Jorge Duenas <jorge@duenas.at>
2021-06-04 09:39:15 +0200

    can parse parameters

--- src/myweb/core.clj
+++ src/myweb/core.clj
@@ -1,10 +1,23 @@
 (ns myweb.core
   (:require [ring.adapter.jetty :as jetty]))
 
+(import '[org.eclipse.jetty.util UrlEncoded MultiMap])
+
+
+(defn parse-query-string
+  ;; source: https://stackoverflow.com/questions/6591604/how-to-parse-url-parameters-in-clojure
+  [query]
+  (let [params (MultiMap.)]
+      (UrlEncoded/decodeTo query params "UTF-8")
+      (into {} params)))
+
+
 (defn myapp [request]
-  {:body "Gello World"
-   :status 200
-   :headers {"Content-Type" "text/html"}})
+  (let [params (parse-query-string (:query-string request))]
+     {:body (format "Gello World %s and %s" (params "str1") (params "str2"))
+      :status 200
+      :headers {"Content-Type" "text/html"}}))
+
 
 (defn -main []
   (jetty/run-jetty myapp {:port 3000}))
